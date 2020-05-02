e99ae60a203138d94dc66d9d483cae37e22dcacd
jakaBlaz <blaz.molicnik@gmail.com>
2020-05-02 20:10:45 +0200

    V0.01

--- pythonCode/main.py
+++ pythonCode/main.py
@@ -4,6 +4,5 @@ import numpy as np
 
 filepath = "D:\Personal\Programiranje\Ledinska_roka\data\OpenBCISession_2020-01-21_17-04-57\OpenBCI-RAW-2020-01-21_17-12-11.txt"
 file = open(filepath,"r")
-vrstice = file.readlines()
-zeljeniInfo = vrstice[-1].split(",")
-print(zeljeniInfo[2])
+while True:
+    continue

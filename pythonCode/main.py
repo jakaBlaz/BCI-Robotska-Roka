bb79387a4fc3176b302e2cba90124a0e79cf2d8d
jakaBlaz <blaz.molicnik@gmail.com>
2020-05-02 19:57:25 +0200

    testna verzija 1.0

--- pythonCode/main.py
+++ pythonCode/main.py
@@ -1,1 +1,9 @@
 #to je glavna skripta
+import numpy as np
+
+
+filepath = "D:\Personal\Programiranje\Ledinska_roka\data\OpenBCISession_2020-01-21_17-04-57\OpenBCI-RAW-2020-01-21_17-12-11.txt"
+file = open(filepath,"r")
+vrstice = file.readlines()
+zeljeniInfo = vrstice[-1].split(",")
+print(zeljeniInfo[2])

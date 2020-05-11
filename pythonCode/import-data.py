3aa36642ab578b3e787f6bf8cd9147befcc3473b
shrekislife27 <oskar.gerzevic@gmail.com>
2020-05-11 17:45:52 +0200

    dejansko nafilan main

--- pythonCode/import-data.py
+++ pythonCode/import-data.py
@@ -2,6 +2,7 @@ import numpy as np
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+
 option = input("Read .txt file or start stream? Type 'txt' or 'stream' >> ")
 altfile = '/Users/iripuga/Documents/1.Delo/404/_bci_/BCI-Robotska-Roka/data/a-very-light-test.txt'
 '''
@@ -17,7 +18,6 @@ if option=='txt':
     filename = askopenfilename()
     f = open(filename)
     root.destroy()
-
     data = np.genfromtxt(filename, delimiter=",", skip_header=6) #prvih 6 vrstic so metapodatki
     print(data)
 elif option == 'stream':
--- pythonCode/main.py
+++ pythonCode/main.py
@@ -1,1 +1,7 @@
 #to je glavna skripta
+import bisijao as BCI
+
+option = input("Read .txt file or start stream? Type 'txt' or 'stream' >> ")
+nepopkolan = BCI.start(option)
+data = BCI.popcol(nepopkolan, 8)
+print(data)

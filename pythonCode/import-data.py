5db89b8a1732a6caf53a55a0a0fb5f4019b2fa94
iripuga <irinej.papuga@gmail.com>
2020-05-16 18:44:39 +0200

    added printout to import-data

--- pythonCode/import-data.py
+++ pythonCode/import-data.py
@@ -2,7 +2,6 @@ import numpy as np
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-
 option = input("Read .txt file or start stream? Type 'txt' or 'stream' >> ")
 altfile = '/Users/iripuga/Documents/1.Delo/404/_bci_/BCI-Robotska-Roka/data/a-very-light-test.txt'
 
@@ -24,4 +23,5 @@ else:
 
     data = np.genfromtxt(filename, delimiter=",", skip_header=6) #prvih 6 vrstic so metapodatki
 
+print('...data imported!')
 f.close()

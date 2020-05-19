15419e4f3048dd97b4c73fcb60d49347dc47f3de
iripuga <irinej.papuga@gmail.com>
2020-05-19 15:44:01 +0200

    WORKING version

--- pythonCode/main.py
+++ pythonCode/main.py
@@ -32,3 +32,5 @@ plt.subplot(4, 1, 4); plt.plot(x, y4)
 plt.show(block=False) # ne blokira zapiranja figure
 plt.pause(3)  # počakam 3s...
 plt.close()   # ... in zdaj jo lahko zaprem.
+
+#še en komentar

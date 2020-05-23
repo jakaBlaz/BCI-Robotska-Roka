45450e35c92f28e1aa04215232e30be777426d8a
NMger34 <nal.mencin@gmail.com>
2020-05-23 12:52:22 +0200

    grafi delajo zdej

--- pythonCode/main.py
+++ pythonCode/main.py
@@ -2,6 +2,9 @@
 import bisijao as bci
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use((r'C:\Users\Nal\RobotskaRoka\BCI-Robotska-Roka\pythonCode\stylelib\bci-style.mplstyle'))
+
+
 
 # Uvoz podatkov
 
@@ -12,49 +15,73 @@ data = np.array(bci.popcol(nepopkolan, 8)) #rešimo se stolpca z datumom - data 
 data = data[0, :, :] #3D matriko damo v 2D
 print(data.shape)
 
-x = data[:, 0]          #prvi kanal graf
-print(x)
 
-y = data[:, 1] # [vrstica, stolpec]
-print(y)
 
-#plt.figure()
-plt.plot(x, y)
 
+
+
+
+
+x = data[:, 0] # indeksi vrstic
+
+
+
+# podatki
+y1 = data[:, 1] # [vrstica, stolpec]
+y2 = data[:, 2]
+y3 = data[:, 3]
+y4 = data[:, 4]
+ax = data[:, 5]
+ay = data[:, 6]
+az = data[:, 7]
+
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['lines.linestyle'] = '--'
+
+
+#plt.rcParams['figure.figsize']  = [100.4, 100.8]
+plt.rcParams['lines.linewidth'] = 3
+plt.rcParams['lines.markersize'] = 29
+plt.rcParams['figure.autolayout'] = True
+#plt.rcParams['xstick.labelsize'] = 16
+#plt.rcParams['ystick.labelsize'] = 16
+
+plt.figure(figsize=(20,15))
+plt.subplot(4, 1, 1); plt.plot(x, y1, color = '#ff8000')          #prvi subplot
 # naming the x axis 
-plt.xlabel('x - indeksi(N)') 
+#plt.xlabel('x - indeksi(N)') 
 # naming the y axis 
-plt.ylabel('y - EEG kanal(uV)') 
-
+#plt.ylabel('y - EEG kanal(uV)') 
 plt.title('Prvi EEG kanal vlomljeno s časom') 
 
-plt.show()
-plt.subplot(4, 1, 1); plt.plot(x, y1)
-plt.subplot(4, 1, 2); plt.plot(x, y2)
-plt.subplot(4, 1, 3); plt.plot(x, y3)
-plt.subplot(4, 1, 4); plt.plot(x, y4)
-
-
-
-x2 = data[:, 0]             #drugi kanal graf
-print(x)
-
-y2 = data[:, 2] # [vrstica, stolpec]
-print(y)
-
-#plt.figure()
-plt.plot(b, 'r')
-
+plt.subplot(4, 1, 2); plt.plot(x, y2, color = 'red')           #drugi subplot
 # naming the x axis 
-plt.xlabel('x2 - indeksi(N)') 
+#plt.xlabel('x - indeksi(N)') 
 # naming the y axis 
-plt.ylabel('y2 - EEG kanal(uV)') 
-
+#plt.ylabel('y - EEG kanal(uV)') 
 plt.title('Drugi EEG kanal vlomljeno s časom') 
 
-plt.show()
 
-plt.subplot(4, 1, 1); plt.plot(x, y1)
-plt.subplot(4, 1, 2); plt.plot(x, y2)
-plt.subplot(4, 1, 3); plt.plot(x, y3)
-plt.subplot(4, 1, 4); plt.plot(x, y4)
+plt.subplot(4, 1, 3); plt.plot(x, y3, color = 'blue')           #tretji subplot
+# naming the x axis 
+#plt.xlabel('x - indeksi(N)') 
+# naming the y axis 
+#plt.ylabel('y - EEG kanal(uV)') 
+plt.title('Tretji EEG kanal vlomljeno s časom') 
+
+plt.subplot(4, 1, 4); plt.plot(x, y4, color = '#bf00ff')           #četrti subplot
+# naming the x axis 
+#plt.xlabel('x - indeksi(N)') 
+# naming the y axis 
+#plt.ylabel('y - EEG kanal(uV)') 
+plt.title('Četri EEG kanal vlomljeno s časom') 
+
+plt.show(block=False) # ne blokira zapiranja figure
+
+
+plt.pause(100000000)  # počakam 3s...
+
+#plt.close()   # ... in zdaj jo lahko zaprem.
+
+#še en komentar
+

710f1d0b45214fbc452f31fc32af5ffae0c5b3c6
iripuga <irinej.papuga@gmail.com>
2020-06-06 16:27:29 +0200

    neki

--- pythonCode/PyDrive/Quickstart.py
+++ pythonCode/PyDrive/Quickstart.py
@@ -6,7 +6,7 @@ gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
-file1 = drive.CreateFile({'title': 'Hello.csv','parents': [{'id': '1eeA-y9DjF4zOuH4z5wkOB5nSehRtGneV'}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+file1 = drive.CreateFile({'title': 'Hello.txt','parents': [{'id': '1eeA-y9DjF4zOuH4z5wkOB5nSehRtGneV'}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
 file1.SetContentString('Hello, World,!') # Set content of the file from given string.
 file1.Upload()
 

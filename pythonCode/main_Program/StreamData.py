52b6d656e10bd83c0c1d7485061ee27891d8d364
jakaBlaz <blaz.molicnik@gmail.com>
2020-07-20 11:29:58 +0200

    connection to Arm

--- pythonCode/main_Program/StreamData.py
+++ pythonCode/main_Program/StreamData.py
@@ -1,10 +1,13 @@
 #to je glavna skripta
 import bisijao as bci
+import sendToArduino as arduino
 import numpy as np
 import matplotlib.pyplot as plt
 from pylsl import StreamInlet, resolve_stream
 import scipy.fftpack as sfp
 import time
+import scipy.signal as signal
+import serial
 
 ### Nastavitve analize signala ###
 EMG_meja = 50 # uV
@@ -12,7 +15,7 @@ fs = 200 #Sample rate v herzih (Ganglion je preko BLE vmesnika omejen na 200Hz)
 N = 200 # Sample size
 toleranca = 0.01 # Med 0 in 1 - določa kakšna odstopanja od EMG_meje spremenijo bool vrednost
 i = 0 # iterator za posodabljanje vzorcev v vektorju signal
-signal = np.zeros(N) # vektor dolžine N, kamor se shranjujejo vzorci 
+#signal = np.zeros(N) # vektor dolžine N, kamor se shranjujejo vzorci 
 flag = False # flag to notify when vector signal is filled with samples
 previousBOOL = False # na začetku je roka odprta - RMS signala je pod EMG_mejo
 lowcut = 1 # V
@@ -41,10 +44,13 @@ inlet1 = StreamInlet(stream1[0])
 #inlet2 = StreamInlet(stream2[0])
 
 # prikažem dolžino vzorčnega vektorja
-data = []
+
 i = 0
-start = time.time()
+
+serial = arduino.initializeServo()
+
 while True:
+    data = []
     # get a new sample (you can also omit the timestamp part if you're not
     # interested in it)
     start = time.time()
@@ -53,12 +59,30 @@ while True:
         data.append(sample1[1])
         i = i+1
     #sample2, timestamp = inlet2.pull_sample()
+
     y = bci.butter_bandpass_filter(data, lowcut, highcut, fs, order=6)
-    y = bci.notch_filter()
+    y = bci.notch_filter(data,f0,Q,fs)
     f, Pxx = signal.periodogram(y,fs = fs,return_onesided = False)
     i = 0
     zanimivo_obmocje = int((f.size/2)*0.2)
-    moc = np.sum(Pxx[0:zanimivo_obmocje]) / podatki.size
+    moc = np.sum(Pxx[0:zanimivo_obmocje]) / N
     print(moc)
-    print(start - time.time())
+    if moc > 100.0:
+        kot = "100"
+        kot = kot.encode(encoding='ascii',errors='strict')
+        serial.write(kot)
+        serial.flush()
+        #preverjanje = serial.readline()
+        #preverjanje = preverjanje.decode()
+        #print(preverjanje)
+    
+    else:
+        kot = "200"
+        kot = kot.encode(encoding='ascii',errors='strict')
+        serial.write(kot)
+        serial.flush()    
+        #preverjanje = serial.readline()
+        #preverjanje = preverjanje.decode()
+        #print(preverjanje)
+    print(time.time()-start)
     time.sleep(1)
